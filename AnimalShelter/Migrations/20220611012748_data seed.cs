32df728b0e1ed96ef18466394a65eebd0f7cb0f5
Mark McConnell <amark.mcconnell@gmail.com>
2022-06-10 18:33:07 -0700

    update Migration & Models

---
+++ AnimalShelter/Migrations/20220611012748_data seed.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class dataseed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "DateAdd", "Gender", "Name", "Neutered", "Species" },
+                values: new object[,]
+                {
+                    { 6, 7, new DateTime(2022, 8, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Female", "Matilda", "Y", "Dog" },
+                    { 2, 10, new DateTime(2022, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Female", "Rexie", "Y", "Dog" },
+                    { 3, 2, new DateTime(2022, 3, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Female", "Matilda", "N", "Cat" },
+                    { 4, 4, new DateTime(2022, 7, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Male", "Pip", "Y", "Bird" },
+                    { 5, 22, new DateTime(2022, 6, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Male", "Bartholomew", "N", "Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6);
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using System;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -12,16 +13,17 @@ namespace AnimalShelter.Models
     }
     public DbSet<Animal> Animals { get; set; }
     
-    // protected override void OnModelCreating(ModelBuilder builder)
-    //     {
-    //     builder.Entity<Animal>()
-    //         .HasData(
-    //             new Animal { AnimalId = 1, Name = "Matilda", Neutered = "Y", Age = 7, Species = "Dog",  Gender = "Female", DateAdd = DateTime.Parse("2022-08-01,")},
-    //             new Animal { AnimalId = 2, Name = "Rexie", Neutered = "Y", Age = 10, Species = "Dog",  Gender = "Female", DateAdd = DateTime.Parse("2022-02-01,")},
-    //             new Animal { AnimalId = 3, Name = "Matilda", Neutered = "N", Age = 2, Species = "Cat",  Gender = "Female", DateAdd = DateTime.Parse("2022-03-01,")},
-    //             new Animal { AnimalId = 4, Name = "Pip", Neutered = "Y", Age = 4, Species = "Bird",  Gender = "Male", DateAdd = DateTime.Parse("2022-07-01,")},
-    //             new Animal { AnimalId = 5, Name = "Bartholomew", Neutered = "N", Age = 22, Species = "Cat",  Gender = "Male", DateAdd = DateTime.Parse("2022-06-01,")}
-    //         );
-    //     }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+					base.OnModelCreating(modelBuilder);
+        	modelBuilder.Entity<Animal>()
+            .HasData(
+                new Animal { AnimalId = 6, Name = "Matilda", Neutered = "Y", Age = 7, Species = "Dog",  Gender = "Female", DateAdd = DateTime.Parse("2022-08-01,")},
+                new Animal { AnimalId = 2, Name = "Rexie", Neutered = "Y", Age = 10, Species = "Dog",  Gender = "Female", DateAdd = DateTime.Parse("2022-02-01,")},
+                new Animal { AnimalId = 3, Name = "Matilda", Neutered = "N", Age = 2, Species = "Cat",  Gender = "Female", DateAdd = DateTime.Parse("2022-03-01,")},
+                new Animal { AnimalId = 4, Name = "Pip", Neutered = "Y", Age = 4, Species = "Bird",  Gender = "Male", DateAdd = DateTime.Parse("2022-07-01,")},
+                new Animal { AnimalId = 5, Name = "Bartholomew", Neutered = "N", Age = 22, Species = "Cat",  Gender = "Male", DateAdd = DateTime.Parse("2022-06-01,")}
+            );
+        }
     }
 }
